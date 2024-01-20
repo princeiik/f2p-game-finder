f51ab31eaceac07f7c37a61b5b6375bc88780edc
Prince Kanu <kanuprince93@yahoo.com>
2024-01-19 19:55:16 -0600

    make code more semantic for clarity

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,9 +6,11 @@ import Footer from '../components/Footer'
 
 function Homepage() {
   return (
-    <>
-      <div className='w-full min-h-screen bg-slate-900'>
+    <div className='min-h-screen flex flex-col'>
+      <header className=''>
         <Navbar />
+      </header>
+      <main className="flex-grow bg-indigo-950">
         <div className='text-center pt-6'>
           <h1 className="text-2xl md:text-4xl font-bold text-slate-100 shadow-white"> Search Free-to-Play Games </h1>
         </div>
@@ -26,11 +28,36 @@ function Homepage() {
         <div className='text-center pt-8'>
           <RandomGame />
         </div>
-        <footer>
-            <Footer />
-        </footer>
-      </div>
-    </>
+      </main>
+      <footer className=''>
+        <Footer />
+      </footer>
+    </div>
+    // <>
+    //   <div className='w-full min-h-screen bg-slate-900'>
+    //     <Navbar />
+    //     <div className='text-center pt-6'>
+    //       <h1 className="text-2xl md:text-4xl font-bold text-slate-100 shadow-white"> Search Free-to-Play Games </h1>
+    //     </div>
+    //     <div>
+    //       <Form />
+    //     </div>
+    //     <div className='text-center text-2xl  text-slate-100 shadow-white'>
+    //       <hr className="my-4 border-slate-100"/>
+    //       <h1 className='py-3'>OR</h1>
+    //       <hr className="my-4 border-slate-100"/>
+    //     </div>
+    //     <div className='text-center pt-6'>
+    //       <h1 className="text-4xl font-bold text-slate-100 shadow-white"> Search a Random Free-to-Play Game</h1>
+    //     </div>
+    //     <div className='text-center pt-8'>
+    //       <RandomGame />
+    //     </div>
+    //     <footer>
+    //         <Footer />
+    //     </footer>
+    //   </div>
+    // </>
   )
 }
 
