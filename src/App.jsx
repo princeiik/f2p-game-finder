774d2a58ea73a5035c40ffac445f6a76205b7ad7
Prince Kanu <kanuprince93@yahoo.com>
2024-05-24 00:26:56 -0500

    set up routing to Home page and Result page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Results from './components/Results'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/results' element={<Results />} />
+      </Routes>
+    </Router>
   )
 }
 
