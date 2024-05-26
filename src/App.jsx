1fdce8fad02f726859dbca9d5ec0ec68e2933155
Prince Kanu <kanuprince93@yahoo.com>
2024-05-26 00:35:50 -0500

    testing homepage with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Results from './components/Results'
+// import Home from './components/Home'
+// import Results from './components/Results'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/results' element={<Results />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
