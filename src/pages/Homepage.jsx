73377ba5025b319ca4eb3fb83c4558fa9b556335
Prince Kanu <kanuprince93@yahoo.com>
2024-05-30 00:34:53 -0500

    add navbar component to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Form from '../components/Form'
+import Navbar from '../components/Navbar'
 
 function Homepage() {
   return (
     <>
+        <Navbar />  
         <h1 className="text-3xl font-bold underline"> Search Free-to-Play Games </h1>
-       <Form />
+        <Form />
     </>
   )
 }
