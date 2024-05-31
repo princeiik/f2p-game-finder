9d8e0e1c260c9d19cd0d56979a4d949c85ae92cd
Prince Kanu <kanuprince93@yahoo.com>
2024-05-31 00:42:20 -0500

    add styling to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,22 +8,75 @@ function Form() {
 
 
     return (
-        <div>
-            <form action="">
-                <div>
-                    <label>Title:</label>
-                </div>
-                <div>
-                    <label>Genre:</label>
-                </div>
-                <div>
-                    <label>Platform:</label>
-                </div>
-                <div>
-                    <button>Search</button>
-                </div>
-            </form>
-        </div>
+        <form>
+            <div className="sm:col-span-3">
+              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                Title
+              </label>
+              <div className="mt-2">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>United States</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                </select>
+              </div>
+              <div className="sm:col-span-3">
+              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                Genre
+              </label>
+              <div className="mt-2">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>United States</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                </select>
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                Platform
+              </label>
+              <div className="mt-2">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>United States</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                </select>
+              </div>
+            </div>
+            </div>
+        </form>
+        // <div>
+        //     <form action="">
+        //         <div>
+        //             <label>Title:</label>
+        //         </div>
+        //         <div>
+        //             <label>Genre:</label>
+        //         </div>
+        //         <div>
+        //             <label>Platform:</label>
+        //         </div>
+        //         <div>
+        //             <button>Search</button>
+        //         </div>
+        //     </form>
+        // </div>
   )
 }
 
