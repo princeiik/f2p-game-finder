0b27955b162b77f60937fa9e2412dbc69b53d52a
Prince Kanu <kanuprince93@yahoo.com>
2024-06-05 20:16:45 -0500

    moved router code outside of app function

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import {
 } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Results from './pages/Results'
+import Navbar from './components/Navbar'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<Homepage />} />
+      <Route path='/results' element={<Results />} />
+    </>
+  )
+)
 
 function App() {
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/results' element={<Results />} />
-      </>
-    )
-  )
 
   return (
     <RouterProvider router={router} />
