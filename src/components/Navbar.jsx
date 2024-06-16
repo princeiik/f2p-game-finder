21d48cfcf3b2dd8fe8b23c73bbcce8b45aa04c83
Prince Kanu <kanuprince93@yahoo.com>
2024-06-16 04:00:08 -0500

    change navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,39 @@
 import React from 'react'
-import controller from '../assets/controller.svg'
 
 function Navbar() {
   return (
-    <section className="relative mx-auto">
-        {/* navbar */}
-      <nav className="flex justify-between bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white w-screen">
-        <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-          <a className="text-3xl font-bold font-heading" href="/">
-            {/* <span>FREE 2 PLAY GAMES</span> */}
-            <img className="h-9" src={controller} alt="logo" />
-            {/* Logo Here. */}
-          </a>
-          {/* Nav Links */}
-          <ul className="hidden md:flex px-4 mx-auto text-2xl font-semibold font-heading space-x-12">
-            <li><a className="hover:text-gray-200" href="/">FREE 2 PLAY GAME FINDER</a></li>
-            {/* <li><a className="hover:text-gray-200" href="/results">Results</a></li> */}
-          </ul>
-          {/* Header Icons */}
-          <div className="hidden xl:flex items-center space-x-5 items-center">
-            {/* <a className="hover:text-gray-200" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+    <header className="bg-gray-800">
+      <nav className="container mx-auto px-6 py-6">
+        <div className="flex items-center justify-between">
+          <div className="text-white font-bold text-xl">
+            <a href="#">Free 2 Play Game Finder</a>
+          </div>
+          <div className="hidden md:block">
+            <ul className="flex items-center space-x-8">
+              <li><a href="#" className="text-white">Home</a></li>
+              {/* <li><a href="#" className="text-white">About</a></li>
+              <li><a href="#" className="text-white">Services</a></li>
+              <li><a href="#" className="text-white">Contact</a></li> */}
+            </ul>
+          </div>
+          <div className="md:hidden">
+            <button className="outline-none mobile-menu-button">
+              <svg className="w-6 h-6 text-white" x-show="!showMenu" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M4 6h16M4 12h16M4 18h16"></path>
               </svg>
-            </a> */}
-            {/* <a className="flex items-center hover:text-gray-200" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-              <span className="flex absolute -mt-5 ml-4">
-                <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-                  </span>
-                </span>
-            </a> */}
-            {/* Sign In / Register */}
-            {/* <a className="flex items-center hover:text-gray-200" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-            </a> */}
+            </button>
           </div>
         </div>
-        {/* Responsive navbar */}
-        <a className="xl:hidden flex mr-6 items-center" href="#">
-          {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg> */}
-          <span className="flex absolute -mt-5 ml-4">
-            <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-            </span>
-          </span>
-        </a>
-        <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-        </a>
+        <div className="mobile-menu hidden md:hidden">
+          <ul className="mt-4 space-y-4">
+            <li><a href="#" className="block px-4 py-2 text-white bg-gray-900 rounded">Home</a></li>
+            {/* <li><a href="#" className="block px-4 py-2 text-white bg-gray-900 rounded">About</a></li>
+            <li><a href="#" className="block px-4 py-2 text-white bg-gray-900 rounded">Services</a></li>
+            <li><a href="#" className="block px-4 py-2 text-white bg-gray-900 rounded">Contact</a></li> */}
+          </ul>
+        </div>
       </nav>
-    </section>
+    </header>
   )
 }
     
