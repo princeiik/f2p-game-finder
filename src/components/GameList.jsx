66728bfe6042c02ade20f3c312efe139bba2c84f
Prince Kanu <kanuprince93@yahoo.com>
2024-06-20 05:13:44 -0500

    change the button color when mouse hovers over it

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -53,7 +53,7 @@ function GameList() {
                   </p>
                 </div>
                 <div className="p-6 pt-0">
-                  <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                  <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:shadow-lg hover:shadow-purple-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                     Read More
                   </button>
                 </div>
