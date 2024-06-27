fb7ddadbd3e9d07ac83e0b6c0734aafba812c0a8
Prince Kanu <kanuprince93@yahoo.com>
2024-06-27 14:59:40 -0500

    make the basic layout of the Gamepage

--- src/pages/Gamepage.jsx
+++ src/pages/Gamepage.jsx
@@ -9,15 +9,27 @@ function Gamepage() {
         <div>
             <Navbar />
             <h1>{game.title}</h1>
+            <div className='flex flex-row flex-wrap'>
+                <div className='basis-1/2 bg-red-500'>
+                    <h2>Game Image and screenshot</h2>
+                </div>
+                <div className='basis-1/2 bg-blue-500'>
+                    <h2>Game information</h2>
+                </div>
+                <div className='basis-full bg-green-500'>
+                    <h2>Description and Game site link</h2>
+                </div>
+            </div>
+            {/* <h1>{game.title}</h1>
             <img src={game.thumbnail} alt={`Image of ${game.title}`} />
             <p>{game.short_description}</p>
-            <p>{game.developer}</p>
+            <p>{game.developer}</p> */}
             {/* Add more game details here */}
         </div>
     )
 }
 
-async function gameLoader( {params }) {
+async function gameLoader({ params }) {
     const { id } = params
     const res = await fetch(`api/game?id=${id}`)
     const data = await res.json()
