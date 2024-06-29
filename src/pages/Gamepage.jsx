ce3a7d3795d5236c36ff152b750077d3b65db9ce
Prince Kanu <kanuprince93@yahoo.com>
2024-06-28 20:06:30 -0500

    edit styling for Gamepage

--- src/pages/Gamepage.jsx
+++ src/pages/Gamepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Carousel from '../components/Carousel'
 
 function Gamepage() {
     const game = useLoaderData()            
@@ -8,16 +9,49 @@ function Gamepage() {
     return (
         <div>
             <Navbar />
-            <h1>{game.title}</h1>
-            <div className='flex flex-row flex-wrap'>
-                <div className='basis-1/2 bg-red-500'>
-                    <h2>Game Image and screenshot</h2>
-                </div>
-                <div className='basis-1/2 bg-blue-500'>
-                    <h2>Game information</h2>
-                </div>
-                <div className='basis-full bg-green-500'>
-                    <h2>Description and Game site link</h2>
+            <h1 className='text-center'>{game.title}</h1>
+            <div className='px-4 py-4'>
+                <div className='grid grid-cols-3 grid-rows-3'>
+                    {/* Image 1 */}
+                    <div className='col-span-2 bg-red-500'>
+                        <img className='p-4' src={game.screenshots[0].image} alt="" />
+                    </div>
+                    <div className='bg-orange-500'>
+                        <p>{game.short_description}</p>
+                        <ul>
+                            <li>Status: {game.status}</li>
+                            <li>Genre: {game.genre}</li>
+                            <li>Platform: {game.platform}</li>
+                            <li>Publisher: {game.publisher}</li>
+                            <li>Developer: {game.developer}</li>
+                            <li>Release Date: {game.release_date}</li>
+                        </ul>
+                    </div>
+                    <div className='bg-yellow-500'>
+                        <p>{game.description}</p>
+                    </div>
+                    {/* Image 2 */}
+                    <div className='col-span-2 bg-green-500'>
+                        <img className='p-4' src={game.screenshots[1].image} alt="" />
+                    </div>
+                    {/* Image 3 */}
+                    <div className='col-span-2 bg-blue-500'>
+                        <img className='p-4' src={game.screenshots[2].image} alt="" />
+                    </div>
+                    <div className='bg-purple-500'>
+                        Minimum System Requirements:
+                        <ul>
+                            <li>{game.minimum_system_requirements.os}</li>
+                            <li>{game.minimum_system_requirements.processor}</li>
+                            <li>{game.minimum_system_requirements.memory}</li>
+                            <li>{game.minimum_system_requirements.graphics}</li>
+                            <li>{game.minimum_system_requirements.storage}</li>
+                        </ul>
+                        <p>Want to learn more about {game.title}?</p>
+                        <button>
+                            <a href={game.game_url}>Click Here</a>
+                        </button>
+                    </div>
                 </div>
             </div>
             {/* <h1>{game.title}</h1>
