5080177de0f563edf3405ada604159792425c479
Prince Kanu <kanuprince93@yahoo.com>
2024-07-27 21:26:48 -0500

    adjust layout size on xl screens

--- src/pages/Gamepage.jsx
+++ src/pages/Gamepage.jsx
@@ -44,7 +44,7 @@ function Gamepage() {
                     <div className='flex flex-col gap-y-4 md:flex-row md:gap-x-4'>
                     </div>
                     <div className='flex flex-col gap-y-4 md:flex-row md:gap-x-4'>
-                        <div className='w-full md:px-28 lg:px-44'>
+                        <div className='w-full md:px-28 lg:px-44 xl:px-72'>
                             <div className='h-full bg-pink-700 rounded-lg overflow-y-auto md:h-full md:rounded-none md:rounded-b-lg'>
                                 <div className='flex flex-col p-4 text-xs gap-y-4 xl:p-6'>
                                     {game.minimum_system_requirements && (
@@ -54,7 +54,7 @@ function Gamepage() {
                                     )}
                                     <div className=''>
                                         {game.minimum_system_requirements && (
-                                            <ul className='pl-4 font-medium space-y-4 md:text-base lg:text-lg lg:space-y-8 xl:pl-8 xl:text-2xl xl:space-y-10 xl:tracking-wide'>
+                                            <ul className='pl-4 font-medium space-y-4 md:text-base lg:text-lg lg:space-y-8 xl:pl-8 xl:text-2xl xl:space-y-10 xl:tracking-wide xl:text-center'>
                                                 {game.minimum_system_requirements.os && (
                                                     <li>OS: <span className='font-normal'>{game.minimum_system_requirements.os}</span></li>
                                                 )}
