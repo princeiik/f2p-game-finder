de1fd0163b02414375b92a7d62c43fb786c37166
Prince Kanu <kanuprince93@yahoo.com>
2024-08-15 16:42:24 -0500

    added styling and centered randomgame component

--- src/components/RandomGame.jsx
+++ src/components/RandomGame.jsx
@@ -6,24 +6,24 @@ function RandomGame() {
     const { games } = useFetchGames()
     const navigate = useNavigate()
 
-    let randomGame = Math.floor(Math.random() * games.length) 
+    let randomizeGame = Math.floor(Math.random() * games.length) 
 
-    function random() {
-        console.log(games[randomGame].id);
+    function randomGame() {
+        console.log(games[randomizeGame].id);
         return navigate(`/results/${games[randomGame].id}`)
     }
 
     return (
         <div>
             <div>
-                <h1 className='text-white'>Pick a game at random</h1>
+                <h1 className='text-white text-sm text-center font-medium leading-6 text-slate-100'>Click to find random free-to-play game</h1>
             </div>
             <div className="p-6 pt-4">
                 <button 
-                    onClick={random}
+                    onClick={randomGame}
                     className='select-none rounded-lg bg-purple-600 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:shadow-lg hover:shadow-purple-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
                     >
-                    Click me
+                    RANDOMIZE
                 </button>
                 {/* <Link
                   to={`/results/${gameID}`}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,12 +9,20 @@ function Homepage() {
       <div className='h-dvh bg-violet-950'>
         <Navbar />
         <div className='text-center pt-6'>
-          <h1 className="text-4xl font-bold text-slate-100 shadow-white "> Search Free-to-Play Games </h1>
+          <h1 className="text-4xl font-bold text-slate-100 shadow-white"> Search Free-to-Play Games </h1>
         </div>
         <div>
           <Form />
-        </div>  
-        <div>
+        </div>
+        <div className='text-center text-2xl  text-slate-100 shadow-white'>
+          <h2>_______________________________________________________________</h2>
+          <h1 className='py-3'>OR</h1>
+          <h2 className=''>‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾</h2>
+        </div>
+        <div className='text-center pt-6'>
+          <h1 className="text-4xl font-bold text-slate-100 shadow-white"> Search a Random Free-to-Play Game</h1>
+        </div>
+        <div className='text-center pt-8'>
           <RandomGame />
         </div>
       </div>
