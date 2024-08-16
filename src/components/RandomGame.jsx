225ff29f1ec2a8cfa6da95577652122cd3cfddac
Prince Kanu <kanuprince93@yahoo.com>
2024-08-16 16:43:00 -0500

    clean up code

--- src/components/RandomGame.jsx
+++ src/components/RandomGame.jsx
@@ -25,12 +25,6 @@ function RandomGame() {
                     >
                     RANDOMIZE
                 </button>
-                {/* <Link
-                  to={`/results/${gameID}`}
-                  className='select-none rounded-lg bg-purple-600 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:shadow-lg hover:shadow-purple-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
-                >
-                  Click me
-                </Link> */}
             </div>
         </div>
     )
