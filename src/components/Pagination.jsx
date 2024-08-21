5e197d73e0b7e7dcace5bdc4a39499d6ca68b370
Prince Kanu <kanuprince93@yahoo.com>
2024-08-21 14:12:56 -0500

    add comments

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,6 +4,7 @@ function Pagination({previousPage, nextPage, currentPage, totalPages}) {
     
     return (
         <div className='flex justify-center py-4'>
+            {/* Previous button */}
             <button
               onClick={previousPage}
               disabled={currentPage === 1}
@@ -12,6 +13,7 @@ function Pagination({previousPage, nextPage, currentPage, totalPages}) {
               Previous
             </button>
             <span className='mx-2 px-4 py-2 text-slate-100'>{currentPage} of {totalPages}</span>
+            {/* Next button */}
             <button
               onClick={nextPage}
               disabled={currentPage === totalPages}
