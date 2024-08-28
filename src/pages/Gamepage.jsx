a4e00db14a9d7fefa3da6525986aeb8c5edb4c2a
Prince Kanu <kanuprince93@yahoo.com>
2024-08-27 21:44:08 -0500

    change layout of game description

--- src/pages/Gamepage.jsx
+++ src/pages/Gamepage.jsx
@@ -21,15 +21,41 @@ function Gamepage() {
                                 <div className='text-xs font-semibold italic text-center pb-4 md:text-lg lg:text-xl xl:text-2xl'>
                                     <p>"{game.short_description}"</p>
                                 </div>
-                                <div className=''>
-                                    <ul className='text-xs space-y-2 font-medium md:text-base md:space-y-4 lg:text-lg lg:space-y-6 lg:space-y-8 xl:pl-6 xl:tracking-wide xl:text-2xl xl:space-y-6'>
+                                <div className='text-xs'>
+                                    <div className='grid grid-cols-1 divide-y'>
+                                        <div className='grid grid-cols-2 py-2'>
+                                            <div>Status:</div>
+                                            <div className='justify-self-end'>{game.status}</div>
+                                        </div>
+                                        <div className='grid grid-cols-2 py-2'>
+                                            <div>Genre</div>
+                                            <div className='justify-self-end'>{game.genre}</div> 
+                                        </div>
+                                        <div className='grid grid-cols-2 py-2'>
+                                           <div>Platform:</div>
+                                            <div className='justify-self-end'>{game.platform}</div> 
+                                        </div>
+                                        <div className='grid grid-cols-2 py-2'>
+                                           <div>Publisher:</div>
+                                            <div className='justify-self-end'>{game.publisher}</div> 
+                                        </div>
+                                        <div className='grid grid-cols-2 py-2'>
+                                            <div>Developer:</div>
+                                            <div className='justify-self-end'>{game.developer}</div>
+                                        </div>
+                                        <div className='grid grid-cols-2 py-2'>
+                                            <div>Release Date:</div>
+                                            <div className='justify-self-end'>{game.release_date}</div>
+                                        </div>
+                                    </div>
+                                    {/* <ul className='text-xs space-y-2 font-medium md:text-base md:space-y-4 lg:text-lg lg:space-y-6 lg:space-y-8 xl:pl-6 xl:tracking-wide xl:text-2xl xl:space-y-6'>
                                         <li>Status: <span className='font-normal'>{game.status}</span></li>
                                         <li>Genre: <span className='font-normal'>{game.genre}</span></li>
                                         <li>Platform: <span className='font-normal'>{game.platform}</span></li>
                                         <li>Publisher: <span className='font-normal'>{game.publisher}</span></li>
                                         <li>Developer: <span className='font-normal'>{game.developer}</span></li>
                                         <li>Release Date: <span className='font-normal'>{game.release_date}</span></li>
-                                    </ul>
+                                    </ul> */}
                                 </div>  
                             </div>
                         </div>
