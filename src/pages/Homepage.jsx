d6c69590779bdac051fcf4db69aaeac4bbcff5c8
Prince Kanu <kanuprince93@yahoo.com>
2024-09-24 15:42:41 -0500

    fix background on small screens v2

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,18 +6,18 @@ import RandomGame from '../components/RandomGame'
 function Homepage() {
   return (
     <>
-      <div className='min-h-screen bg-slate-900'>
+      <div className='w-full min-h-screen bg-slate-900'>
         <Navbar />
         <div className='text-center pt-6'>
-          <h1 className="text-4xl font-bold text-slate-100 shadow-white"> Search Free-to-Play Games </h1>
+          <h1 className="text-2xl md:text-4xl font-bold text-slate-100 shadow-white"> Search Free-to-Play Games </h1>
         </div>
         <div>
           <Form />
         </div>
         <div className='text-center text-2xl  text-slate-100 shadow-white'>
-          <h2>_______________________________________________________________</h2>
+          <hr className="my-4 border-slate-100"/>
           <h1 className='py-3'>OR</h1>
-          <h2 className=''>‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾</h2>
+          <hr className="my-4 border-slate-100"/>
         </div>
         <div className='text-center pt-6'>
           <h1 className="text-4xl font-bold text-slate-100 shadow-white"> Search a Random Free-to-Play Game</h1>
