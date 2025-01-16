66f19f405cdbb7e61b37e31cb1af65889c6f5269
Prince Kanu <kanuprince93@yahoo.com>
2025-01-16 00:48:00 -0600

    create a basic layout for footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-lg shadow-purple-500/40'>
+        <h1>THIS IS A FOOTER</h1>
+    </footer>
+  )
+}
+
+export default Footer
